3b33ebb54a2303d92a66678efa0edb5c11b76b51
Amar Singh <gujraal2006@gmail.com>
2024-04-27 07:36:13 +0530

    services and contact pages rough update
    27/4

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,17 +1,17 @@
 exports.home = async (req, res, next) => {
-  res.status(200).render("index");
+  res.status(200).render("index",{title:"DavmoTech | Home"});
 };
 
 exports.about = async (req, res, next) => {
-  res.status(200).render("about");
+  res.status(200).render("about",{title:"DavmoTech | About"});
 };
 
 exports.services = async (req, res, next) => {
-  res.status(200).render("services");
+  res.status(200).render("services",{title:"DavmoTech | Services"});
 };
 
 exports.contact = async (req, res, next) => {
-  res.status(200).render("contact");
+  res.status(200).render("contact",{title:"DavmoTech | Contact"});
 };
 
 exports.login = async (req, res, next) => {
