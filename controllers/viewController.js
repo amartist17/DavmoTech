de5f76483fb615ba9c5e7113ad72d551a4d38a1f
Amar Singh <gujraal2006@gmail.com>
2024-04-29 07:28:09 +0530

    all pages added

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -10,8 +10,49 @@ exports.services = async (req, res, next) => {
   res.status(200).render("services",{title:"DavmoTech | Services"});
 };
 
+exports.customSoftwareDevelopment = async (req, res, next) => {
+  res.status(200).render("services/custom-software-development", {title: "DavmoTech | Custom Software Development"});
+};
+
+exports.webApplicationDevelopment = async (req, res, next) => {
+  res.status(200).render("services/web-application-development", {title: "DavmoTech | Web Application Development"});
+};
+
+exports.uiUxDesign = async (req, res, next) => {
+  res.status(200).render("services/ui-ux-design", {title: "DavmoTech | UI/UX Design"});
+};
+
+exports.staffAugmentation = async (req, res, next) => {
+  res.status(200).render("services/staff-augmentation", {title: "DavmoTech | Staff Augmentation"});
+};
+
+exports.webPortalDevelopment = async (req, res, next) => {
+  res.status(200).render("services/web-portal-development", {title: "DavmoTech | Web Portal Development"});
+};
+
+exports.mobileAppDevelopment = async (req, res, next) => {
+  res.status(200).render("services/mobile-app-development", {title: "DavmoTech | Mobile App Development"});
+};
+
+exports.eCommerceSolutions = async (req, res, next) => {
+  res.status(200).render("services/e-commerce-solutions", {title: "DavmoTech | E-Commerce Solutions"});
+};
+
+
+exports.portfolio = async (req, res, next) => {
+  res.status(200).render("portfolio",{title:"DavmoTech | Portfolio"});
+};
+
+exports.blog = async (req, res, next) => {
+  res.status(200).render("blog",{title:"DavmoTech | Blog"});
+};
+
 exports.contact = async (req, res, next) => {
   res.status(200).render("contact",{title:"DavmoTech | Contact"});
+};
+
+exports.terms = async (req, res, next) => {
+  res.status(200).render("terms",{title:"DavmoTech | Terms"});
 };
 
 exports.login = async (req, res, next) => {
--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -6,8 +6,18 @@ const router = express.Router({ mergeParams: true });
 router.get("/", viewController.home);
 router.get("/about", viewController.about);
 router.get("/services", viewController.services);
+router.get("/services/custom-software-development", viewController.customSoftwareDevelopment);
+router.get("/services/web-application-development", viewController.webApplicationDevelopment);
+router.get("/services/ui-ux-design", viewController.uiUxDesign);
+router.get("/services/staff-augmentation", viewController.staffAugmentation);
+router.get("/services/web-portal-development", viewController.webPortalDevelopment);
+router.get("/services/mobile-app-development", viewController.mobileAppDevelopment);
+router.get("/services/e-commerce-solutions", viewController.eCommerceSolutions);
+
 router.get("/contact", viewController.contact);
-// router.get("/portfolio", viewController.portfolio);
+router.get("/blog", viewController.blog);
+router.get("/portfolio", viewController.portfolio);
+router.get("/terms", viewController.terms);
 router.get("/login", viewController.login);
 // router.get("/dashboard", viewController.dashboard);
 
