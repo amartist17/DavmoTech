bd9ac169be41c8c0bdb281964ffcabaa12a876f9
Amar Singh <gujraal2006@gmail.com>
2024-05-09 09:54:00 +0530

    update 9 may
    -home hero section updated
    -some of our fines works in 1 line
    -sub headings size increase
    -hover strike through
    - less repetition of lets work together
    - email issue on contact page solved

--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const viewController = require('../controllers/viewController')
+const apiController = require('../controllers/apiController')
 const router = express.Router({ mergeParams: true });
 
 
@@ -9,10 +10,10 @@ router.get("/services", viewController.services);
 router.get("/services/custom-software-development", viewController.customSoftwareDevelopment);
 router.get("/services/web-application-development", viewController.webApplicationDevelopment);
 router.get("/services/ui-ux-design", viewController.uiUxDesign);
-router.get("/services/staff-augmentation", viewController.staffAugmentation);
-router.get("/services/web-portal-development", viewController.webPortalDevelopment);
+// router.get("/services/staff-augmentation", viewController.staffAugmentation);
+// router.get("/services/web-portal-development", viewController.webPortalDevelopment);
 router.get("/services/mobile-app-development", viewController.mobileAppDevelopment);
-router.get("/services/e-commerce-solutions", viewController.eCommerceSolutions);
+// router.get("/services/e-commerce-solutions", viewController.eCommerceSolutions);
 
 router.get("/contact", viewController.contact);
 router.get("/blog", viewController.blog);
@@ -20,6 +21,12 @@ router.get("/portfolio", viewController.portfolio);
 router.get("/terms", viewController.terms);
 router.get("/login", viewController.login);
 // router.get("/dashboard", viewController.dashboard);
+
+
+router.post("/subscribe", apiController.addToNewsletter);
+router.post("/needhelp", apiController.needhelp);
+router.post("/contact", apiController.contactform);
+
 
 
 
